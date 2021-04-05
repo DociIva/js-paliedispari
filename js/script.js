0e4d8f1fa511ca38bc3a2d6cec208f93927c3024
DociIva <doci.iva@hotmail.com>
2021-04-05 22:45:53 +0200

    prova due

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ Stabiliamo se la somma dei due numeri
  è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+/*
 
 // 1 - chiedi all'utente di inserire una parola 
 var accessoParola = prompt ("Inserisca una parola");
@@ -37,9 +38,54 @@ function utenteCapovolto (word){
 }
 // si fà la decisione delle varie oopportunità
 if (accessoParola === parolaCapovolta){
- console.log("La parola è palindorma " + accessoParola);
+ console.log("La parola è palindroma " + accessoParola);
 } else if (accessoParola !== parolaCapovolta){
     console.log("La parola non è palindroma ");
 }
+*/
+
+/*
+2- Pari e Dispari
+
+L’utente sceglie pari o dispari
+e inserisce un numero da 1 a 5.
+
+Generiamo un numero random (sempre da 1 a 5)
+per il computer (usando una funzione).
+Sommiamo i due numeri
+
+Stabiliamo se la somma dei due numeri
+ è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+var persona = parseInt( prompt ("Scegli pari o dispari"));
+
+var persona2 = parseInt( prompt ("Scegli un numero da 1 a 5"));
+
+var sommaComputerPersona = persona2 + genRandom() + pariDispari();
+console.log("Somma " , + sommaComputerPersona);
 
 
+// controllare se è pari o Dispari
+
+var controlloPariDispari = sommaComputerPersona + pariDispari();
+
+// funzione per generazione del numero
+function genRandom (num1){
+    var num1 = Math.floor(Math.random() * 5) + 1;
+
+    var controllo1 = "";
+
+    // ciò che deve ritornare 
+    return controllo1;
+}
+
+// funzione per controllare se è pari o dispari
+
+function pariDispari (num){ 
+    if(num % 2 === 0){
+        console.log("Pari" , num);
+    }else{
+        console.log("Dispari", num);
+    }
+}
