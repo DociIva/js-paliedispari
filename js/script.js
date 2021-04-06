20e7ef825bcad3f6610a42ade3a0d73814b64518
DociIva <doci.iva@hotmail.com>
2021-04-06 17:32:50 +0200

    cor

--- js/script.js
+++ js/script.js
@@ -60,8 +60,47 @@ Dichiariamo chi ha vinto.
 */
 
 // crea una variabile per la scelta del giocatore
-var sceltaGiocatore = prompt('Gentilmente scegli tra : Pari o Dispari').toLocaleLowerCase.trim();
+var sceltaGiocatore = prompt('Gentilmente scegli tra : Pari o Dispari').toLocaleLowerCase().trim();
 console.log("La scelta del giocatore è : ", sceltaGiocatore);
 
 // variabile per il numero del giocatore
-var numeroGiocatore = parseInt(prompt("Gentilmente ora iniserisci\n un numero da 1 a 5 ")).trim();
+var numeroGiocatore = parseInt(prompt("Gentilmente ora iniserisci\n un numero da 1 a 5 "));
+console.log("Il numero del giocatore è : ", numeroGiocatore );
+
+// Numero Opponente
+//prima la variabile la mettiamo a 0, poi dopo mettiamo getrandomnumeri con il range da (1 -5)
+// N.B. (1,) <-- la virgola deve stare ataccata al primo numero
+var numeroOpponente = getRandomNumber(1, 5);
+console.log("Il numero opponente è : " , numeroOpponente);
+
+//Somma dei nuemri per vedere chi vince
+var somma = numeroGiocatore + numeroOpponente;
+
+
+// Controllo somma del numero -- una delle due stringhe di sotto saranno il risultato di di questo
+//               invochi la funzione
+var risultato = controlloPariDispari(somma);
+
+// Vincitore --  (decisione)
+if (risultato === sceltaGiocatore){
+    console.log("Vince il giocatore");
+} else {
+    console.log("Vince il computer");
+}
+
+// ora generiamo un nuemro random con un range ( 1 - 5)
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min ;
+}
+
+
+// Check = controllo, per il numero se è pari o dispari 
+
+function controlloPariDispari(numero) {
+    // decisione
+    if(numero % 2 === 0) {
+       return "pari";
+    }
+    // non metto else if o else perchè tanto dopo return il codice non viene più eseguito
+    return "dispari"
+}
